67b48c0588be78477672c3c2b63da9427df7f650
Zritel <manin.tech@yandex.ru>
2022-06-07 23:41:12 +0300

    Template function for filtering matched_documents.

--- y_cpp_my.cpp
+++ y_cpp_my.cpp
@@ -1,3 +1,5 @@
+// search_server_s1_t2_v2.cpp
+
 #include <algorithm>
 #include <cmath>
 #include <iostream>
@@ -9,9 +11,7 @@
 
 using namespace std;
 
-
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
-const double EPSILON = 1e-6;
 
 string ReadLine() {
     string s;
@@ -81,57 +81,73 @@ public:
             });
     }
 
-    vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status = DocumentStatus::ACTUAL) const {
+    //*****************************
+    template <typename FilterFunction>
+    vector<Document> FindTopDocuments(const string& raw_query, FilterFunction ff) const {
         const Query query = ParseQuery(raw_query);
-        auto matched_documents = FindAllDocuments(query, status);
+        auto matched_documents = FindAllDocuments(query);
 
         sort(matched_documents.begin(), matched_documents.end(),
             [](const Document& lhs, const Document& rhs) {
-                return (abs(lhs.relevance - rhs.relevance) > EPSILON && lhs.relevance > rhs.relevance) || 
-                    (abs(lhs.relevance - rhs.relevance) < EPSILON && lhs.rating > rhs.rating);
+                if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                    return lhs.rating > rhs.rating;
+                }
+                else {
+                    return lhs.relevance > rhs.relevance;
+                }
             });
-        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
-            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+
+        //delete matched_documents by the logic of ff
+        map<int, DocumentStatus> statuses;
+        for (const auto& doc : matched_documents) {
+            statuses[doc.id] = documents_.at(doc.id).status;
         }
-        return matched_documents;
+        vector<Document> filtered_docs;
+        for (const auto& md : matched_documents) {
+            if (ff(md.id, statuses[md.id], md.rating)) { filtered_docs.push_back({ md.id, md.relevance, md.rating }); }
+        }
+
+        if (filtered_docs.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            filtered_docs.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return filtered_docs;
+    }
+
+    //*****************************
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+
+        auto ff = [](int document_id, DocumentStatus status, int rating) { return status == DocumentStatus::ACTUAL; };
+        vector<Document> filtered_docs = FindTopDocuments(raw_query, ff);
+        return filtered_docs;
+    }
+
+    int GetDocumentCount() const {
+        return static_cast<int>(documents_.size());
     }
 
     tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query, int document_id) const {
-        //В первом элементе кортежа верните все плюс-слова запроса, содержащиеся в документе. Слова не должны дублироваться. Пусть они будут отсортированы по возрастанию.
-        //Если документ не соответствует запросу(нет пересечений по плюс - словам или есть минус - слово), вектор слов нужно вернуть пустым.
-        tuple<vector<string>, DocumentStatus> pwords_status;
-        vector<string> pwords;
-
         const Query query = ParseQuery(raw_query);
-        for (const string& word : query.minus_words) {
-            if (word_to_document_freqs_.count(word) == 0) {
-                continue;
-            }
-            else {
-                if (word_to_document_freqs_.at(word).count(document_id) > 0) {
-                    return tuple(pwords, documents_.at(document_id).status);
-                }
-            }
-        }
-
+        vector<string> matched_words;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
-            else {
-                if (word_to_document_freqs_.at(word).count(document_id) > 0) {
-                    pwords.push_back(word);
-                }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.push_back(word);
             }
         }
-        sort(pwords.begin(), pwords.end());
-        pwords_status = tuple(pwords, documents_.at(document_id).status);
-        return pwords_status;
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.clear();
+                break;
+            }
+        }
+        return { matched_words, documents_.at(document_id).status };
     }
 
-    int GetDocumentCount() {
-        return static_cast<int>(documents_.size());
-    }
 private:
     struct DocumentData {
         int rating;
@@ -210,10 +226,10 @@ private:
 
     // Existence required
     double ComputeWordInverseDocumentFreq(const string& word) const {
-        return log(documents_.size() * 1.0 / word_to_document_freqs_.at(word).size());
+        return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
     }
 
-    vector<Document> FindAllDocuments(const Query& query, DocumentStatus status) const {
+    vector<Document> FindAllDocuments(const Query& query) const {
         map<int, double> document_to_relevance;
         for (const string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
@@ -221,9 +237,9 @@ private:
             }
             const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
             for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
-                if (documents_.at(document_id).status == status) {
-                    document_to_relevance[document_id] += term_freq * inverse_document_freq;
-                }
+
+                document_to_relevance[document_id] += term_freq * inverse_document_freq;
+
             }
         }
 
@@ -246,9 +262,10 @@ private:
         }
         return matched_documents;
     }
-
-
 };
+
+
+// ==================== для примера =========================
 
 
 void PrintDocument(const Document& document) {
@@ -266,8 +283,22 @@ int main() {
     search_server.AddDocument(0, "белый кот и модный ошейник"s, DocumentStatus::ACTUAL, { 8, -3 });
     search_server.AddDocument(1, "пушистый кот пушистый хвост"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
     search_server.AddDocument(2, "ухоженный пёс выразительные глаза"s, DocumentStatus::ACTUAL, { 5, -12, 2, 1 });
+    search_server.AddDocument(3, "ухоженный скворец евгений"s, DocumentStatus::BANNED, { 9 });
 
-    for (const Document& document : search_server.FindTopDocuments("ухоженный кот"s)) {
+    cout << "ACTUAL by default:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s)) {
         PrintDocument(document);
     }
+
+    cout << "ACTUAL:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, [](int document_id, DocumentStatus status, int rating) { return status == DocumentStatus::ACTUAL; })) {
+        PrintDocument(document);
+    }
+
+    cout << "Even ids:"s << endl;
+    for (const Document& document : search_server.FindTopDocuments("пушистый ухоженный кот"s, [](int document_id, DocumentStatus status, int rating) { return document_id % 2 == 0; })) {
+        PrintDocument(document);
+    }
+
+    return 0;
 }
