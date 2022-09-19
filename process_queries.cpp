2d556ad31bf015108d88094827afa444876e30a8
Zritel <manin.tech@yandex.ru>
2022-09-19 14:21:18 +0300

    Execution::par is added to trancform.

--- process_queries.cpp
+++ process_queries.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <string>
+#include <execution>
 
 #include "process_queries.h"
 #include "search_server.h"
@@ -8,7 +9,8 @@ std::vector<std::vector<Document>> ProcessQueries(
     const SearchServer& search_server,
     const std::vector<std::string>& queries) {
     std::vector<std::vector<Document>> result(queries.size());
-    std::transform(queries.begin(), queries.end(), result.begin(),
+    std::transform(std::execution::par,
+        queries.begin(), queries.end(), result.begin(),
         [search_server](std::string q) { return search_server.FindTopDocuments(q); });
     return result;
 };
