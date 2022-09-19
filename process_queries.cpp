dbb597cebe3cdd1581074cc2d48871fc8bb2258a
Zritel <manin.tech@yandex.ru>
2022-09-19 13:44:06 +0300

    process_queries (transform): looks like working version. Not yet tested in the platform

---
+++ process_queries.cpp
@@ -0,0 +1,14 @@
+#include <algorithm>
+#include <string>
+
+#include "process_queries.h"
+#include "search_server.h"
+
+std::vector<std::vector<Document>> ProcessQueries(
+    const SearchServer& search_server,
+    const std::vector<std::string>& queries) {
+    std::vector<std::vector<Document>> result(queries.size());
+    std::transform(queries.begin(), queries.end(), result.begin(),
+        [search_server](std::string q) { return search_server.FindTopDocuments(q); });
+    return result;
+};
